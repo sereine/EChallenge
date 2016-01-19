1995f5c3769e878428111570f57e2b7f713708c2
sereine <amal_vie88@hotmail.com>
2016-01-19 21:32:37 +0000

    AbstractDao

---
+++ src/main/java/com/EChallenge/dao/GeneriqueDao.java
@@ -0,0 +1,15 @@
+package com.EChallenge.dao;
+
+import java.io.Serializable;
+
+public interface GeneriqueDao <T, PK extends Serializable>{
+	
+	PK create(T newInstance);
+
+    T read(PK id);
+
+    void update(T transientObject);
+    
+    void delete(T persistentObject);
+
+}
---
+++ src/main/java/com/EChallenge/dao/GeneriqueDaoImp.java
@@ -0,0 +1,34 @@
+package com.EChallenge.dao;
+
+import java.io.Serializable;
+
+public class GeneriqueDaoImp<T, PK extends Serializable>  
+	implements GeneriqueDao<T, PK> {
+	
+	private Class<T> type;
+
+    public GeneriqueDaoImp(Class<T> type) {
+        this.type = type;
+    }
+
+	public PK create(T newInstance) {
+		return (PK) getSession().save(o);
+		
+	}
+
+	public T read(PK id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void update(T transientObject) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void delete(T persistentObject) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
